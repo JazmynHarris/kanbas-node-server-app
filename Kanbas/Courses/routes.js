719eb67538818b8b723a1129c5e72228843ab7eb
Jazmyn Harris <jazmynharris@Jazmyns-MacBook-Pro.local>
2024-08-14 06:30:38 -0400

    committing

--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -1,32 +1,41 @@
-import * as Database from "../Database/index.js";
+import courses from "../Database/courses.js";
+
 export default function CourseRoutes(app) {
     app.put("/api/courses/:id", (req, res) => {
         const { id } = req.params;
-        const course = req.body;
-        Database.courses = Database.courses.map((c) =>
-            c._id === id ? { ...c, ...course } : c
-        );
+        const updatedCourse = req.body;
+
+        const courseIndex = courses.findIndex((c) => c._id === id);
+        if (courseIndex === -1) {
+            return res.status(404).send({ message: "Course not found" });
+        }
+
+        courses[courseIndex] = { ...courses[courseIndex], ...updatedCourse };
         res.sendStatus(204);
     });
 
     app.delete("/api/courses/:id", (req, res) => {
         const { id } = req.params;
-        Database.courses = Database.courses.filter((c) => c._id !== id);
+        const courseIndex = courses.findIndex((c) => c._id === id);
+
+        if (courseIndex === -1) {
+            return res.status(404).send({ message: "Course not found" });
+        }
+
+        courses.splice(courseIndex, 1);
         res.sendStatus(204);
     });
 
     app.post("/api/courses", (req, res) => {
-        const course = {
+        const newCourse = {
             ...req.body,
-            _id: new Date().getTime().toString()
+            _id: new Date().getTime().toString(), // Generate a new unique ID
         };
-        Database.courses.push(course);
-        res.send(course);
+        courses.push(newCourse);
+        res.status(201).json(newCourse);
     });
 
     app.get("/api/courses", (req, res) => {
-        const courses = Database.courses;
-        res.send(courses);
+        res.json(courses);
     });
-    
 }
