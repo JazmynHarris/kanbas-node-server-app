fa0fd20250a2228c7e5cd47499c700c12349f7c1
Jazmyn Harris <jazmynrh12@gmail.com>
2024-08-16 19:17:33 -0400

    adding

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import AssignmentRoutes from './Kanbas/Assignments/routes.js';
 import cors from "cors";
 import mongoose from "mongoose";
 import UserRoutes from './Users/routes.js';
+import session from "express-session";
 
 // Initialize Express app
 const app = express();
@@ -15,19 +16,41 @@ const allowedOrigins = ["http://localhost:3000", "https://classy-brioche-97316a.
 const CONNNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
 mongoose.connect(CONNNECTION_STRING);
 
-app.use(cors({
-    credentials: true,
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            return callback(null, true);
-        } else {
-            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-    }
-}));
+// app.use(cors({
+//     credentials: true,
+//     origin: (origin, callback) => {
+//         if (!origin || allowedOrigins.includes(origin)) {
+//             return callback(null, true);
+//         } else {
+//             const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+//             return callback(new Error(msg), false);
+//         }
+//     }
+// }));
 
-// app.use(cors());
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.NETLIFY_URL || "http://localhost:3000",
+    })
+);
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET || "kanbas",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+        domain: process.env.NODE_SERVER_DOMAIN,
+    };
+}
+app.use(session(sessionOptions));
+
+
+
 
 // Middleware to parse JSON bodies
 app.use(express.json());
